e65b6e562d3558b9704ed841b1ba2fcca4350d72
SpJnv <115528444+SpJnv@users.noreply.github.com>
2024-07-24 16:09:43 +0530

    Add files via upload

---
+++ median_of_array.py
@@ -0,0 +1,16 @@
+# import the numpy package
+import numpy as np
+
+# given array
+x_odd = np.array([1, 2, 3, 4, 5, 6, 7])
+
+# calculating the median of the given array
+median = np.median(x_odd)
+
+# print the meadian of the array
+print(f"median of the array is : {median}")
+
+# Output
+"""
+median of the array is : 4.0
+"""
---
+++ standard_deviation_of_array.py
@@ -0,0 +1,24 @@
+# importing the numpy package
+import numpy as np
+
+# Given data
+arr = np.array([20, 2, 7, 1, 34])
+
+# find the mean of the array
+mean_arr = np.mean(arr)
+
+# printing the mean of array
+print(f"mean of the array = {mean_arr}")
+
+# calculating the standard deviation
+standard_dev = np.std(arr)
+
+# printing the standard deviation
+print(f"standard deviation of the array = {standard_dev}")
+
+
+# Output
+"""
+mean of the array = 12.8
+standard deviation of the array = 12.576167937809991
+"""
